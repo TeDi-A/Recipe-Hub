f8619a9ea9b25532c2b125fac9a8a0d2c1a2d64f
TeDi-A <tediadjene@gmail.com>
2025-03-10 14:46:22 +0100

    Update images

--- src/layouts/HeroHeader.jsx
+++ src/layouts/HeroHeader.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import Loader from "./StyledHeader";
+// import pastaImg  from "public/assets/images/pasta-image.jpg";
 
 const HeroHeader = () => {
+  // const background = pastaImg
 
   return (
     <div>
-      <div className="hero-img" >
-        <img src="./public/assets/images/pasta-image.jpg" alt="" />
+      <div className="hero-img">
+        <img src="public/images/pasta-image.jpg" alt="" />
 
         <div className="hero-text">
-    
           <h1>Checkout</h1>
           <Loader />
           <h1>Recipes</h1>
